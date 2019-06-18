f46a1cf7e99159c93b5bf4af5dbb538823aaa6ea
Luan Dias <luan.dias@rockcontent.com>
2019-06-18 19:31:27 -0300

    Set initial structure

--- __init__.py
+++ __init__.py
@@ -1,7 +1,21 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+from flask import (Flask, g, render_template, flash, redirect, url_for, abort, jsonify, request, session)
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import RPi.GPIO as io
+from config import Config
+
 app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello, I love Digital Ocean!"
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes
+
 if __name__ == "__main__":
+    io.setmode(io.BCM)
+    io.setup(16, io.OUT)
+    io.setup(19, io.OUT)
+    io.setup(20, io.OUT)
+    io.setup(21, io.OUT)
     app.run()
---
+++ routes.py
@@ -0,0 +1,5 @@
+from vox import app
+
+@app.route("/")
+def hello():
+    return "Jimmy Neutron"
