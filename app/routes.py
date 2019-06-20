2f3847d59a88ee80df0f0fee8e2c349747a46770
Lu Dias <lucandidoan@gmail.com>
2019-06-20 04:12:54 -0300

    Change static file directory

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,11 @@ from app import app
 from flask import (render_template, redirect, url_for)
 
 import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
 
 light_status = {16:True, 19:True, 20:True, 21:True}
 
@@ -9,7 +14,7 @@ light_status = {16:True, 19:True, 20:True, 21:True}
 def index():
     return render_template('room.html', light_status=light_status)
 
-@app.route("/lights/<status>/<id>")
+@app.route("/lights/<status>/<int:id>")
 def lights(status, id):
     if status == "on":
         GPIO.output(id, True)
