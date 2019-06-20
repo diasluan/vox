74a21c264dae97c32c9ff741ecd503e34b608eac
Luan Dias <luan.dias@rockcontent.com>
2019-06-20 03:03:41 -0300

    Add first physical actions on room template

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,20 @@
 from app import app
+from flask import (render_template, redirect, url_for)
+
+import RPi.GPIO as GPIO
+
+light_status = {16:True, 19:True, 20:True, 21:True}
 
 @app.route("/")
-def hello():
-    return "Jimmy Neutron"
+def index():
+    return render_template('room.html', light_status=light_status)
+
+@app.route("/lights/<status>/<id>")
+def lights(status, id):
+    if status == "on":
+        GPIO.output(id, True)
+        light_status[id] = True
+    elif status == "off":
+        GPIO.output(id, False)
+        light_status[id] = False
+    return redirect(url_for('index'))
